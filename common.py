709ed3582c2e377c5ba8b55b9417ff3e9a694cb3
Mary <>
2021-02-18 17:35:59 -0500

    remove pycache

---
+++ common.py
@@ -0,0 +1,9 @@
+from app import create_app
+from app.extensions import db
+db.create_all(app=create_app())
+
+from app.models import Users, Appointments
+
+user = Users(username="User1", password="kitty", email="hi@gmail.com")
+db.session.add(user)
+db.session.commit
